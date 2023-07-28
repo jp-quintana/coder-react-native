0a89ccb8f826ab5c37ea8529cdf7dec797d5ef4d
jp-quintana <juanquintana1996@gmail.com>
2023-07-28 18:56:03 -0300

    Update Babel Config

--- desafio-10y11/babel.config.js
+++ desafio-10y11/babel.config.js
@@ -1,6 +1,24 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          alias: {
+            components: './src/components',
+            screens: './src/screens',
+            navigation: './src/navigation',
+            data: './src/data',
+            helpers: './src/helpers',
+            store: './src/store',
+            features: './src/features',
+            hooks: './src/hooks',
+          },
+        },
+      ],
+    ],
   };
 };
